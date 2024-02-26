f469300fe5bb808d836e40cc809cdfba9252587d
emmieIO <markonuoha6@gmail.com>
2024-02-26 20:58:14 +0100

    creating server-2

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,8 @@
 This module contains the index
 """
 from flask import Blueprint
-# Import all views here to register them
-from api.v1.views.index import *
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+# Import all views here to register them
+from api.v1.views.index import *
