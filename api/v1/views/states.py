e68313af50fbb84b80cf42c5f1a42fd1cb518019
emmieIO <markonuoha6@gmail.com>
2024-02-27 00:57:55 +0100

    State Crud

---
+++ api/v1/views/states.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""This is the states view"""
+from flask import Flask, jsonify, request, abort
+from api.v1.views import app_views
+from models import storage, State
+
+
+@app_views.route('/states', methods=['GET'])
+def get_states():
+    """Get all states"""
+    states = storage.all(State).values()
+    return jsonify([state.to_dict() for state in states])
+
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    "Get a single state"
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    """Delete State"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/states', methods=['POST'])
+def create_state():
+    """Create state"""
+    if not request.json:
+        abort(400, "Not a JSON")
+    if 'name' not in request.json:
+        abort(400, "Missing name")
+    data = request.get_json()
+    new_state = State(**data)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    """Update State"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    if not request.json:
+        abort(400, "Not a JSON")
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict()), 200
