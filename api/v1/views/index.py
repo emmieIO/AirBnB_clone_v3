064db0eef1286cd3f3f0883efa5862dafb4b4494
emmieIO <markonuoha6@gmail.com>
2024-02-27 00:38:28 +0100

    counting models

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """
 This module contains the index.py
 """
+from models import storage
 from flask import jsonify
 from . import app_views
 
@@ -9,3 +10,15 @@ from . import app_views
 @app_views.route("/status", methods=["GET"])
 def get_status():
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", methods=["GET"])
+def count():
+    """Returns the numbers of the models"""
+    return jsonify({
+  "amenities": storage.count("Amenity"), 
+  "cities": storage.count("City"), 
+  "places": storage.count("Place"), 
+  "reviews": storage.count("Review"), 
+  "states": storage.count("State"), 
+  "users": storage.count("User")
+})
