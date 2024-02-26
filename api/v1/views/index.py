e6e996433fc8485e52a14dcdbd65d72e2078a8f7
emmieIO <markonuoha6@gmail.com>
2024-02-27 00:43:41 +0100

    counting models

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,10 +16,10 @@ def get_status():
 def count():
     """Returns the numbers of the models"""
     return jsonify({
-  "amenities": storage.count("Amenity"), 
-  "cities": storage.count("City"), 
-  "places": storage.count("Place"), 
-  "reviews": storage.count("Review"), 
-  "states": storage.count("State"), 
-  "users": storage.count("User")
-})
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
