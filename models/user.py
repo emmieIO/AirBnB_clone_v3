1b82322057ac649a91053db7b001dffaa57a44a8
emmieIO <markonuoha6@gmail.com>
2024-02-27 02:50:58 +0100

    solving advanced tasks

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,8 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        
+    @password.setter
+    def password(self, pwd):
+        """hashing password values"""
+        self.password = md5(pwd.encode()).hexdigest()
